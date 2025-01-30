8650194c81b89cdfcb45332a87240f2dc26aa192
Josef Kudera <46950237+kudj@users.noreply.github.com>
2025-01-30 08:38:24 +0100

    formatted by ruff

--- src/component.py
+++ src/component.py
@@ -19,8 +19,8 @@ from configuration import Configuration
 from client.api_client import AdformClient
 
 DUCK_DB_MAX_MEMORY = "400MB"
-DUCK_DB_DIR = os.path.join(os.environ.get('TMPDIR', '/tmp'), 'duckdb')
-FILES_TEMP_DIR = os.path.join(os.environ.get('TMPDIR', '/tmp'), 'files')
+DUCK_DB_DIR = os.path.join(os.environ.get("TMPDIR", "/tmp"), "duckdb")
+FILES_TEMP_DIR = os.path.join(os.environ.get("TMPDIR", "/tmp"), "files")
 
 STATE_AUTH_ID = "auth_id"
 STATE_REFRESH_TOKEN = "#refresh_token"
@@ -69,31 +69,32 @@ class Component(ComponentBase):
                 raise UserException(f"Failed to download file: {str(e)}")
 
         for prefix in datasets:
-            downloaded_files = [f for f in filtered_files if f['name'].startswith(prefix)]
+            downloaded_files = [f for f in filtered_files if f["name"].startswith(prefix)]
             if downloaded_files:
                 logging.info(f"Processing dataset: {prefix}")
                 self.save_to_table(prefix, downloaded_files, file_charset, custom_pkeys, incremental)
 
         if meta_files:
-            client.download_file({'id': 'meta__zip', 'name': 'meta.zip', 'setup': setup_id}, FILES_TEMP_DIR)
-            self.unzip_file(f"{FILES_TEMP_DIR}/meta.zip", os.path.join(FILES_TEMP_DIR, 'meta'))
+            client.download_file({"id": "meta__zip", "name": "meta.zip", "setup": setup_id}, FILES_TEMP_DIR)
+            self.unzip_file(f"{FILES_TEMP_DIR}/meta.zip", os.path.join(FILES_TEMP_DIR, "meta"))
             for dim in meta_files:
                 logging.info(f"Processing meta file: {dim}")
                 self.save_metadata_to_table(dim)
         print("Component finished successfully")
 
     def save_to_table(self, prefix, downloaded_files, file_charset, custom_pkeys, incremental):
-        if file_charset == "UTF-8": # if using UTF-8 we can load directly to DuckDB which handles gzip
+        if file_charset == "UTF-8":  # if using UTF-8 we can load directly to DuckDB which handles gzip
             self.duck.execute(f"CREATE VIEW {prefix} AS SELECT * FROM '{FILES_TEMP_DIR}/{prefix}_*.csv.gz'")
 
-        else: # if not UTF-8 we need to first ungzip and convert to UTF-8 and then pass to DuckDB
-            to_process = [f['name'] for f in downloaded_files if f['name'].startswith(prefix)]
+        else:  # if not UTF-8 we need to first ungzip and convert to UTF-8 and then pass to DuckDB
+            to_process = [f["name"] for f in downloaded_files if f["name"].startswith(prefix)]
             unzipped = self.ungzip_convert_to_utf8(to_process, file_charset, FILES_TEMP_DIR)
             self.duck.execute(f"CREATE VIEW {prefix} AS SELECT * FROM '{unzipped}/{prefix}_*.csv'")
 
         table_meta = self.duck.execute(f"""DESCRIBE {prefix};""").fetchall()
-        schema = OrderedDict({c[0]: ColumnDefinition(data_types=BaseType(dtype=self.convert_base_types(c[1])))
-                              for c in table_meta})
+        schema = OrderedDict(
+            {c[0]: ColumnDefinition(data_types=BaseType(dtype=self.convert_base_types(c[1]))) for c in table_meta}
+        )
 
         primary_key = None
         if custom_pkeys:
@@ -101,12 +102,9 @@ class Component(ComponentBase):
         elif schema.get("GUID"):
             primary_key = ["GUID"]
 
-        out_table = self.create_out_table_definition(f"{prefix}.csv",
-                                                     schema=schema,
-                                                     primary_key=primary_key,
-                                                     incremental=incremental,
-                                                     has_header=True
-                                                     )
+        out_table = self.create_out_table_definition(
+            f"{prefix}.csv", schema=schema, primary_key=primary_key, incremental=incremental, has_header=True
+        )
 
         try:
             self.duck.execute(f"COPY {prefix} TO '{out_table.full_path}' (HEADER, DELIMITER ',', FORCE_QUOTE *)")
@@ -121,16 +119,15 @@ class Component(ComponentBase):
             self.duck.execute(f"CREATE VIEW {view_name} AS SELECT * FROM '{FILES_TEMP_DIR}/meta/{dim}.json'")
 
             table_meta = self.duck.execute(f"""DESCRIBE {view_name};""").fetchall()
-            schema = OrderedDict({c[0]: ColumnDefinition(data_types=BaseType(dtype=self.convert_base_types(c[1])))
-                                 for c in table_meta})
+            schema = OrderedDict(
+                {c[0]: ColumnDefinition(data_types=BaseType(dtype=self.convert_base_types(c[1]))) for c in table_meta}
+            )
 
             primary_key = ["id"] if schema.get("id") else None
 
-            out_table = self.create_out_table_definition(f"meta-{dim}.csv",
-                                                         schema=schema,
-                                                         primary_key=primary_key,
-                                                         has_header=True
-                                                         )
+            out_table = self.create_out_table_definition(
+                f"meta-{dim}.csv", schema=schema, primary_key=primary_key, has_header=True
+            )
 
             self.duck.execute(f"COPY {view_name} TO '{out_table.full_path}' (HEADER, DELIMITER ',', FORCE_QUOTE *)")
 
@@ -143,18 +140,19 @@ class Component(ComponentBase):
 
     @staticmethod
     def ungzip_convert_to_utf8(in_files: list[str], source_encoding: str, input_dir: str) -> str:
-
-        ungzipped_dir = os.path.join(input_dir, 'unzipped')
+        ungzipped_dir = os.path.join(input_dir, "unzipped")
         os.makedirs(ungzipped_dir, exist_ok=True)
 
         for filename in in_files:
             input_path = os.path.join(input_dir, filename)
-            output_filename = filename[:-3] if filename.endswith('.gz') else filename
+            output_filename = filename[:-3] if filename.endswith(".gz") else filename
             output_path = os.path.join(ungzipped_dir, output_filename)
 
-            if filename.endswith('.gz'):
-                with gzip.open(input_path, 'rt', encoding=source_encoding) as f_in, \
-                        open(output_path, 'w', encoding='utf-8') as f_out:
+            if filename.endswith(".gz"):
+                with (
+                    gzip.open(input_path, "rt", encoding=source_encoding) as f_in,
+                    open(output_path, "w", encoding="utf-8") as f_out,
+                ):
                     while True:
                         chunk = f_in.read(1024 * 1024)
                         if not chunk:
@@ -170,18 +168,28 @@ class Component(ComponentBase):
 
     @staticmethod
     def convert_base_types(dtype: str) -> SupportedDataTypes:
-        if dtype in ['TINYINT', 'SMALLINT', 'INTEGER', 'BIGINT', 'HUGEINT',
-                     'UTINYINT', 'USMALLINT', 'UINTEGER', 'UBIGINT', 'UHUGEINT']:
+        if dtype in [
+            "TINYINT",
+            "SMALLINT",
+            "INTEGER",
+            "BIGINT",
+            "HUGEINT",
+            "UTINYINT",
+            "USMALLINT",
+            "UINTEGER",
+            "UBIGINT",
+            "UHUGEINT",
+        ]:
             return SupportedDataTypes.INTEGER
-        elif dtype in ['REAL', 'DECIMAL']:
+        elif dtype in ["REAL", "DECIMAL"]:
             return SupportedDataTypes.NUMERIC
-        elif dtype == 'DOUBLE':
+        elif dtype == "DOUBLE":
             return SupportedDataTypes.FLOAT
-        elif dtype == 'BOOLEAN':
+        elif dtype == "BOOLEAN":
             return SupportedDataTypes.BOOLEAN
-        elif dtype in ['TIMESTAMP', 'TIMESTAMP WITH TIME ZONE']:
+        elif dtype in ["TIMESTAMP", "TIMESTAMP WITH TIME ZONE"]:
             return SupportedDataTypes.TIMESTAMP
-        elif dtype == 'DATE':
+        elif dtype == "DATE":
             return SupportedDataTypes.DATE
         else:
             return SupportedDataTypes.STRING
@@ -213,28 +221,28 @@ class Component(ComponentBase):
 
     def _get_oauth(self, state_file_auth_id, state_file_refresh_token, encrypted_data, client_id, client_secret):
         refresh_token = self._get_refresh_token(
-            state_file_auth_id,
-            state_file_refresh_token,
-            encrypted_data,
-            self.credentials
+            state_file_auth_id, state_file_refresh_token, encrypted_data, self.credentials
         )
         response = self._request_new_token(client_id, client_secret, refresh_token)
         return response["access_token"], response["refresh_token"]
 
     def _request_new_token(self, client_id, client_secret, refresh_token):
         try:
-            response = requests.post(ENDPOINT_TOKEN, data={
-                'client_id': client_id,
-                'client_secret': client_secret,
-                'grant_type': 'refresh_token',
-                'refresh_token': refresh_token,
-                'scope': 'offline_access'  # is required to get new token via refresh token
-            })
+            response = requests.post(
+                ENDPOINT_TOKEN,
+                data={
+                    "client_id": client_id,
+                    "client_secret": client_secret,
+                    "grant_type": "refresh_token",
+                    "refresh_token": refresh_token,
+                    "scope": "offline_access",  # is required to get new token via refresh token
+                },
+            )
             response.raise_for_status()
             return response.json()
 
         except HTTPError as e:
-            raise UserException(f'Failed to fetch token {str(e)}') from e
+            raise UserException(f"Failed to fetch token {str(e)}") from e
 
     @staticmethod
     def _get_refresh_token(auth_id, refresh_token, encrypted_data, credentials):
@@ -252,10 +260,7 @@ class Component(ComponentBase):
         if not self.environment_variables.stack_id:
             logging.debug("Running locally, storing statefile directly.")
 
-            self.write_state_file({
-                STATE_AUTH_ID: self.credentials.get("id", ""),
-                STATE_REFRESH_TOKEN: refresh_token
-            })
+            self.write_state_file({STATE_AUTH_ID: self.credentials.get("id", ""), STATE_REFRESH_TOKEN: refresh_token})
             return
 
         logging.debug("Saving new refresh token to state using Keboola API.")
@@ -267,22 +272,23 @@ class Component(ComponentBase):
             return
 
         new_state = {
-            "component": {
-                STATE_AUTH_ID: self.credentials.get("id", ""),
-                STATE_REFRESH_TOKEN: encrypted_refresh_token
-            }}
+            "component": {STATE_AUTH_ID: self.credentials.get("id", ""), STATE_REFRESH_TOKEN: encrypted_refresh_token}
+        }
         try:
-            self.update_config_state(component_id=self.environment_variables.component_id,
-                                     configurationId=self.environment_variables.config_id,
-                                     state=new_state,
-                                     branch_id=self.environment_variables.branch_id)
+            self.update_config_state(
+                component_id=self.environment_variables.component_id,
+                configurationId=self.environment_variables.config_id,
+                state=new_state,
+                branch_id=self.environment_variables.branch_id,
+            )
         except requests.exceptions.RequestException:
-            logging.warning("Storage API (update config state)"
-                            "is unavailable. Skipping token save at the beginning of the run.")
+            logging.warning(
+                "Storage API (update config state)is unavailable. Skipping token save at the beginning of the run."
+            )
             return
 
     def _get_storage_token(self) -> str:
-        token = self.configuration.parameters.get('#storage_token') or self.environment_variables.token
+        token = self.configuration.parameters.get("#storage_token") or self.environment_variables.token
         if not token:
             raise UserException("Cannot retrieve storage token from env variables and/or config.")
         return token
@@ -293,45 +299,38 @@ class Component(ComponentBase):
         params = {
             "componentId": self.environment_variables.component_id,
             "projectId": self.environment_variables.project_id,
-            "configId": self.environment_variables.config_id
+            "configId": self.environment_variables.config_id,
         }
         headers = {"Content-Type": "text/plain"}
 
-        response = requests.post(url,
-                                 data=token,
-                                 params=params,
-                                 headers=headers)
+        response = requests.post(url, data=token, params=params, headers=headers)
         response.raise_for_status()
         return response.text
 
     @backoff.on_exception(backoff.expo, requests.exceptions.RequestException, max_tries=5)
-    def update_config_state(self, component_id, configurationId, state, branch_id='default'):
+    def update_config_state(self, component_id, configurationId, state, branch_id="default"):
         if not branch_id:
-            branch_id = 'default'
+            branch_id = "default"
 
-        region = os.environ.get('KBC_STACKID', 'connection.keboola.com')
+        region = os.environ.get("KBC_STACKID", "connection.keboola.com")
 
-        url = f'https://{region}/v2/storage/branch/{branch_id}' \
-              f'/components/{component_id}/configs/' \
-              f'{configurationId}/state'
+        url = (
+            f"https://{region}/v2/storage/branch/{branch_id}/components/{component_id}/configs/{configurationId}/state"
+        )
 
-        parameters = {'state': json.dumps(state)}
-        headers = {'Content-Type': 'application/x-www-form-urlencoded', 'X-StorageApi-Token': self._get_storage_token()}
-        response = requests.put(url,
-                                data=parameters,
-                                headers=headers)
+        parameters = {"state": json.dumps(state)}
+        headers = {"Content-Type": "application/x-www-form-urlencoded", "X-StorageApi-Token": self._get_storage_token()}
+        response = requests.put(url, data=parameters, headers=headers)
         response.raise_for_status()
 
     @staticmethod
     def init_duckdb() -> DuckDBPyConnection:
         """
-                Returns connection to temporary DuckDB database
-                """
+        Returns connection to temporary DuckDB database
+        """
         os.makedirs(DUCK_DB_DIR, exist_ok=True)
         # TODO: On GCP consider changin tmp to /opt/tmp
-        config = dict(temp_directory=DUCK_DB_DIR,
-                      threads="1",
-                      max_memory=DUCK_DB_MAX_MEMORY)
+        config = dict(temp_directory=DUCK_DB_DIR, threads="1", max_memory=DUCK_DB_MAX_MEMORY)
         conn = duckdb.connect(config=config)
 
         return conn
@@ -349,9 +348,9 @@ class Component(ComponentBase):
     def filter_files_by_date_and_dataset(files, start_date, end_date, datasets):
         filtered_files = []
         for file in files:
-            file_date = datetime.strptime(file['createdAt'], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
+            file_date = datetime.strptime(file["createdAt"], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
             if (end_date is None and start_date <= file_date) or (start_date <= file_date <= end_date):
-                if any(file['name'].startswith(prefix) for prefix in datasets):
+                if any(file["name"].startswith(prefix) for prefix in datasets):
                     filtered_files.append(file)
         return filtered_files
 
