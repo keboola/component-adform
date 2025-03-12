0d3f03ea87632109c6b7f9b50b2bbd6e6ac8257b
Josef Kudera <46950237+kudj@users.noreply.github.com>
2025-03-12 17:17:06 +0100

    flake

--- src/component.py
+++ src/component.py
@@ -323,7 +323,7 @@ class Component(ComponentBase):
 
     @backoff.on_exception(backoff.expo, requests.exceptions.RequestException, max_tries=5)
     def encrypt(self, token: str) -> str:
-        region = os.environ.get("KBC_STACKID").replace("connection.","")
+        region = os.environ.get("KBC_STACKID").replace("connection.", "")
         url = f"https://encryption.{region}/encrypt"
         params = {
             "componentId": self.environment_variables.component_id,
